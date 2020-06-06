62a105122bab16d1352e4daeadf57d6110d02f28
Yevhenii Prontsevych <prontsevychev@gmail.com>
2020-06-06 14:57:30 +0300

    1. Initial project.
    Create base project. Update settings.
    Add docker and docker-compose configuration.

---
+++ todo_test/settings/dev.py
@@ -0,0 +1,4 @@
+from todo_test.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
---
+++ todo_test/settings/production.py
@@ -0,0 +1,6 @@
+from todo_test.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+STATIC_ROOT = BASE_DIR('static_content', 'static')
